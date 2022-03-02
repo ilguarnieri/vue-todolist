737fa769cd8ff175ba745d60e90e4febfaef0d4c
ilguarnieri <guarnieriangelo91ag@gmail.com>
2022-03-02 13:13:36 +0100

    Completato

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,37 @@
 const app = new Vue({
-    el:'app',
+    el:'#app',
     data: {
-
+        newTodo: '',
+        todoList: [
+            {
+                text: 'Fare la spesa',
+                done: false,
+            },
+            {
+                text: 'Sistemare la cameretta',
+                done: true,
+            },
+            {
+                text: 'Fare i compiti',
+                done: false,
+            }
+        ]
     },
     methods: {
-        
+        addTodo: function(){
+            if(this.newTodo.trim().length){
+                this.todoList.push({
+                    text: this.newTodo,
+                    done: false,
+                });
+            }
+            this.newTodo = '';
+        },
+        deleteTodo: function(i){
+            this.todoList.splice(i,1);
+        },
+        changeDone: function(i){
+            this.todoList[i].done = !this.todoList[i].done;
+        }
     }
 })
